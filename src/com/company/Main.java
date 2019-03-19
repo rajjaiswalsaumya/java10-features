0d24e0ce8571e6ce3fd65549849bb39c6c9f5eab
RAJ JAISWAL <rajjaiswalsaumya@gmail.com>
2019-03-19 09:02:59 +0530

    var for string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String s = "Java 10!!";
+        var s = "Java 10!!";
         System.out.println(s);
     }
 }

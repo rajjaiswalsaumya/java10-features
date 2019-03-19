c9f4e8649c09409087dfe50c6324089bbb8716ee
RAJ JAISWAL <rajjaiswalsaumya@gmail.com>
2019-03-19 09:07:00 +0530

    Method params

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,12 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        var s = "Java 10!!";
-        System.out.println(s);
-    }
+	public static void main(String[] args) {
+		var s = getValue(10);
+		System.out.println(s);
+	}
+
+	public static String getValue(Integer a) {
+		return String.format("Java %d", a);
+	}
 }
